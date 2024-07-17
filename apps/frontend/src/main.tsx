1d1e269b2df69bead6d731eb25e7944b5dda9f9c
ReQ1964 <slomianykacper4@gmail.com>
2024-07-17 16:09:18 +0200

    feat: :package: add and configure i18next package

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './index.css';
 import '@radix-ui/themes/styles.css';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import './libs/i18n/config';
 
 const el = document.getElementById('root');
 if (el) {
   const root = createRoot(el);
   root.render(
     <React.StrictMode>
-      <App />
+      <Suspense fallback="Loading...">
+        <App />
+      </Suspense>
     </React.StrictMode>,
   );
 } else {
